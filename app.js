4d4a117bc60337f9ddfb7d8744c983b8be1a537f
binamralamsal <binamralamsal@gmail.com>
2024-11-15 18:09:10 +0545

    Built-in Modules: events

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-const fs = require("fs");
+// events module is an important module of Node.js
+// many internal modules of Node.js uses it.
+// It allows you to handle and emit events
 
-// We used fs in previous section but you might miss using async/await. This video is for that.
-// fs package was introduced when Node.js was released which means, promises or async/await wasn't a feature at that time.
-// so, they used callback functions at that time.
-// to use promises version of fs. You can use fs.promises
+// This returns EventEmitter class, that's why I am using Pascal convention.
+const EventEmitter = require("events");
 
-// Since it's a promise version, you can use .then() or .catch()
-fs.promises
-  .readdir(".")
-  .then((files) => console.log(files))
-  .catch((err) => console.error(err));
+// This is an object based on EventEmitter.
+// EventEmitter is a class which defines properties and methods that should be inside an event.
+// This is like a blueprint
+// You can create different objects using the same class which has different properties and methods.
+// we are creating one below.
+const emitter = new EventEmitter();
 
-// or you can simply use async/await. async/await is just wrapper around .then() and .catch()
-// I am using Immediately Invoked Functions to use async/await as you can't use on top of the file.
-// Remember that, it is just a limitation of commonjs.
-// When we use es modules in future, you can use async/await on top of file if you have v14.8.0 and later.
-(async () => {
-  try {
-    const content = await fs.promises.readFile("./folder/child/test.txt");
-    console.log(content.toString());
-  } catch (err) {
-    console.error(err);
-  }
-})();
+// This defines handler for an event.
+// We are defining handler for "greet" event.
+// You can think it like having handler for click events in JavaScript.
+// this is same as emitter.addListener
+// emitter.on("greet", () => {
+//   console.log("Hello World");
+// });
 
-// Are you tired of using fs.promises everytime?
-// You can actually just import with fs/promises
-// const fs = require("fs/promises")
-// Now, you don't need to use fs.promises everytime.
+// This is used to emit an event. It's like signaling the event to do something.
+// emitter.emit("greet");
+
+// You can also pass arguments while emitting.
+// emitter.on("greet", (age, name) => {
+//   console.log("Hello World", age, name);
+// });
+
+// emitter.emit("greet", 23, "Binamra");
+
+// but it's best idea to take a single argument as an object.
+emitter.on("greet", (arg) => {
+  console.log("Hello " + arg.name);
+});
+
+emitter.emit("greet", { name: "Binamra" });
