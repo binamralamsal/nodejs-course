af04c1ff80075268d70552dfd18a6b2e3f0d0586
binamralamsal <binamralamsal@gmail.com>
2024-11-15 15:44:55 +0545

    Creating Modules: Named Export

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const add = require("./math");
+// You can either get all in a variable
+// const math = require("./math");
+// and use like
 
-console.log(add(1, 2));
+// console.log(math.add(1, 2));
+
+// or destructure them like this
+const { add, subtract, PI } = require("./math");
+
+console.log(add(5, 3));
+console.log(subtract(10, 4));
+console.log(PI);
