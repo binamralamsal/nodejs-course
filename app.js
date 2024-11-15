7cf040b5f643ee247f85eb07d1f15cfc276d9955
binamralamsal <binamralamsal@gmail.com>
2024-11-15 20:56:19 +0545

    ES Modules: Caveats

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-import { add, subtract, PI } from "./math.js";
+// There are some features and caveats when using ES Modules.
 
-console.log(add(1, 2));
-console.log(subtract(4, 2));
-console.log(PI);
+// After v14.8 onward, you can use top-level await.
 
-// OR
-// import * as math from "./math.js";
+const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+const json = await response.json();
+console.log(json);
 
-// console.log(math.add(1, 2));
-// console.log(math.subtract(4, 2));
-// console.log(math.PI);
+// Some caveats are: module, __dirname, and __filename aren't available with ES Module.
+// You most probably don't need module but you might need __dirname, and __filename.
+// Fortunately, After Node.js 20.11.0, we have access to __dirname, and __filename via import.meta
+console.log(import.meta.dirname);
+console.log(import.meta.filename);
+
+// if you are using lower versions then you can use this:
+// import path from "path";
+
+// const __filename = new URL(import.meta.url).pathname; // import.meta.url was available in previous versions too.
+// const __dirname = path.dirname(__filename);
+// console.log({ __dirname, __filename });
