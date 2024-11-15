c70bedc519a41af5d8e30f91b10194ba0146a719
binamralamsal <binamralamsal@gmail.com>
2024-11-15 17:02:49 +0545

    Built-in Modules: os

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-const path = require("path");
+const os = require("os");
 
-// There are few constants added by Node.js when you are using commonjs. Note, these are only available in commonjs
-console.log(__filename);
-console.log(__dirname);
+const architecture = os.arch();
+const platform = os.platform();
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
 
-// It gives details of the path given.
-console.log(path.parse(__filename));
-
-// You can join path like this, why is this useful? Because, Windows uses \ for separating path segments
-// but linux uses / for separating path segments. It uses the one based on operating system.
-const filePath = path.join("folder", "child", "test.txt");
-
-// This helps to get full absolute path based on our project directory.
-// Note: It doesn't check whether the file or folder exists.
-const resolvedPath = path.resolve(filePath);
-const extname = path.extname(filePath);
-const basename = path.basename(filePath);
-const dirname = path.dirname(filePath);
-// This gives most of the details given above in an object.
-const parsedPath = path.parse(filePath);
-
-console.log({
-  filePath,
-  resolvedPath,
-  extname,
-  basename,
-  dirname,
-  parsedPath,
-  separator: path.sep, // This shows platform specific separator
-});
+console.log({ architecture, platform, totalMemory, freeMemory });
