9ec81bb0a324fe17c4c48da4497b8c399879737b
binamralamsal <binamralamsal@gmail.com>
2024-11-20 21:20:35 +0545

    Express: Handling 404 page

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
 import express from "express";
 import { env } from "./env.js";
+import path from "path";
 
 const app = express();
 
 app.use(express.static("public"));
-// app.use(express.urlencoded());
 app.use(express.urlencoded({ extended: true }));
-// when you use extended: true, it uses https://www.npmjs.com/package/qs (qs) library for parsing query strings
-// It has extended support for parsing req.body;
-// you can pass as an object, arrays, and so on, let's see an example.
-// got to index.html and upate all name to user[email] and user[message]
-// now you can see difference between extended and without extended.
 
-// If you are using default GET method for form, then you can use app.get
-// and access using req.query as GET form sends data in URL query.
-// req.query can be used for any search params that you pass.
-// app.get("/contact", (req, res) => {
-//   const { name, message } = req.query;
-//   console.log(name, message);
-//   res.redirect("/");
-// });
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
 
-app.post("/contact", (req, res) => {
-  // for post request in form, you can access via req.body;
-  // make sure to use express.urlencoded() middleware
-  console.log(req.body);
-  res.redirect("/");
+app.use((req, res) => {
+  // return res.status(404).send("<h1>404 Page</h1>")
+
+  return res
+    .status(404)
+    .sendFile(path.join(import.meta.dirname, "views", "404.html"));
 });
 
 app.listen(env.PORT, () => {
