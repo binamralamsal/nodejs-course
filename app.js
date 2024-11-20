261f607a43aed89d7641707f2bde4556eb3ae660
binamralamsal <binamralamsal@gmail.com>
2024-11-20 21:15:06 +0545

    Express: Form submissions

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 import express from "express";
 import { env } from "./env.js";
-import path from "path";
 
 const app = express();
 
-// to add middleware like express.static() you have to pass it inside app.use()
-// it requires a folder from your current directory, which for us is public.
-// since we have index.html inside public, we don't need separate file to handle homepage.
-// It will use index.html
-// You can also add images inside this folder and you will see that you can access it using /file-name.extension
-// which means we can use css file now.
 app.use(express.static("public"));
+// app.use(express.urlencoded());
 app.use(express.urlencoded({ extended: true }));
+// when you use extended: true, it uses https://www.npmjs.com/package/qs (qs) library for parsing query strings
+// It has extended support for parsing req.body;
+// you can pass as an object, arrays, and so on, let's see an example.
+// got to index.html and upate all name to user[email] and user[message]
+// now you can see difference between extended and without extended.
 
-app.get("/products", (req, res) => {
-  // query parameters or search parameters are optional parameters which you can access in any route
-  // you can pass query parameters like this /products?page=3
-  // to pass multiple query parameters you can separate all of them with &
-  // /products?page=3&limit=10
-  // then you can access query parameters using req.query in express.js
-  console.log(req.query);
-  return res.send("<h1>Products Page</h1>");
+// If you are using default GET method for form, then you can use app.get
+// and access using req.query as GET form sends data in URL query.
+// req.query can be used for any search params that you pass.
+// app.get("/contact", (req, res) => {
+//   const { name, message } = req.query;
+//   console.log(name, message);
+//   res.redirect("/");
+// });
+
+app.post("/contact", (req, res) => {
+  // for post request in form, you can access via req.body;
+  // make sure to use express.urlencoded() middleware
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.listen(env.PORT, () => {
